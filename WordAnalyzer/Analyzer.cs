2f2fc1b3d4506f09377d807147765608768f2b2f
Geoff Bones <geoff.bones@siemens.com>
2016-03-11 15:42:04 +0000

    Solution using linq

--- WordAnalyzer/Analyzer.cs
+++ WordAnalyzer/Analyzer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace WordAnalyzer
 {
@@ -10,7 +11,26 @@ namespace WordAnalyzer
         /// <param name="input"></param>
         public AnalysisResults Analyze(IEnumerable<string> input)
         {
-            return new AnalysisResults();
+            var words = input.ToList();
+            if (words.Count == 0) return new AnalysisResults();
+
+            var results = new AnalysisResults
+            {
+                LongestWordLength = words.Select(word => word.Length).Max(),
+                MostCommonlyUsedWords = MostCommonlyUsedWordsIn(words),
+                WordCount = words.Count
+            };
+            return results;
+        }
+
+        private static IEnumerable<string> MostCommonlyUsedWordsIn(IEnumerable<string> words)
+        {
+            var x = words.GroupBy(word => word)
+                    .Select(wordWithCounts => new {word = wordWithCounts.Key, count = wordWithCounts.Count()})
+                    .ToList();
+            var maxWordFrequency = x.Max(xx => xx.count);
+            var mostCommonlyUsedWords = x.Where(xx => xx.count == maxWordFrequency).Select(xx => xx.word);
+            return mostCommonlyUsedWords;
         }
     }
 }
