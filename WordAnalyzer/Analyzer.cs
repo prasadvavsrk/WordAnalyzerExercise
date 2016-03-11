df0319014957f97e9f51ac02e8e8be92b6ba3b11
Geoff Bones <geoff.bones@siemens.com>
2016-03-11 16:22:50 +0000

    Extract class for finding the most commonly used words

--- WordAnalyzer/Analyzer.cs
+++ WordAnalyzer/Analyzer.cs
@@ -20,50 +20,9 @@ namespace WordAnalyzer
                 results.WordCount++;
                 results.LongestWordLength = Math.Max(word.Length, results.LongestWordLength);
             }
-            results.MostCommonlyUsedWords = MostCommonlyUsedWordsIn(words);
+            var mostCommonlyUsedsWordFinder = new MostCommonlyUsedWordFinder(words);
+            results.MostCommonlyUsedWords = mostCommonlyUsedsWordFinder.MostCommonlyUsedWordsIn();
             return results;
-        }
-
-        private static IEnumerable<string> MostCommonlyUsedWordsIn(IEnumerable<string> words)
-        {
-            var frequencies = CountWordFrequencies(words);
-            var highestFrequency = FindHighestFrequency(frequencies);
-            var mostCommonlyUsedWords = FindWordsWithFrequencyEqualTo(frequencies, highestFrequency);
-
-            return mostCommonlyUsedWords;
-        }
-
-        private static IEnumerable<string> FindWordsWithFrequencyEqualTo(Dictionary<string, int> frequencies, int highestFrequency)
-        {
-            var mostCommonlyUsedWords = new List<string>();
-            foreach (var frequency in frequencies)
-            {
-                if (frequency.Value == highestFrequency)
-                    mostCommonlyUsedWords.Add(frequency.Key);
-            }
-            return mostCommonlyUsedWords;
-        }
-
-        private static int FindHighestFrequency(Dictionary<string, int> frequencies)
-        {
-            var highestFrequency = 0;
-            foreach (var frequency in frequencies)
-            {
-                highestFrequency = Math.Max(highestFrequency, frequency.Value);
-            }
-            return highestFrequency;
-        }
-
-        private static Dictionary<string, int> CountWordFrequencies(IEnumerable<string> words)
-        {
-            var frequencies = new Dictionary<string, int>();
-            foreach (var word in words)
-            {
-                int frequencyOfThisWord;
-                frequencies.TryGetValue(word, out frequencyOfThisWord);
-                frequencies[word] = frequencyOfThisWord + 1;
-            }
-            return frequencies;
         }
     }
 }
---
+++ WordAnalyzer/MostCommonlyUsedWordFinder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordAnalyzer
+{
+    public class MostCommonlyUsedWordFinder
+    {
+        private readonly List<string> m_Words;
+
+        public MostCommonlyUsedWordFinder(List<string> words)
+        {
+            m_Words = words;
+        }
+
+        private Dictionary<string, int> m_Frequencies;
+
+        public IEnumerable<string> MostCommonlyUsedWordsIn()
+        {
+            m_Frequencies = WordFrequencies(m_Words);
+            var highestFrequency = FindHighestFrequency();
+            var mostCommonlyUsedWords = FindWordsWithFrequencyEqualTo(highestFrequency);
+            return mostCommonlyUsedWords;
+        }
+
+        private IEnumerable<string> FindWordsWithFrequencyEqualTo(int highestFrequency)
+        {
+            var mostCommonlyUsedWords = new List<string>();
+            foreach (var frequency in m_Frequencies)
+            {
+                if (frequency.Value == highestFrequency)
+                    mostCommonlyUsedWords.Add(frequency.Key);
+            }
+            return mostCommonlyUsedWords;
+        }
+
+        private int FindHighestFrequency()
+        {
+            var highestFrequency = 0;
+            foreach (var frequency in m_Frequencies)
+            {
+                highestFrequency = Math.Max(highestFrequency, frequency.Value);
+            }
+            return highestFrequency;
+        }
+
+        private Dictionary<string, int> WordFrequencies(IEnumerable<string> words)
+        {
+            var frequencies = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                int frequencyOfThisWord;
+                frequencies.TryGetValue(word, out frequencyOfThisWord);
+                frequencies[word] = frequencyOfThisWord + 1;
+            }
+            return frequencies;
+        }
+    }
+}
