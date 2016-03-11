06aca7009050762c6250f84b40e51ae9ec36c7ad
Geoff Bones <geoff.bones@siemens.com>
2016-03-11 14:22:52 +0000

    Add additional sord sources

--- WordAnalyzer/WordSource1.cs
+++ WordAnalyzer/WordSource1.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace WordAnalyzer
 {
-    public class WordSource1
+    public class WordSource1 : IWordSource
     {
         public IEnumerable<string> Read()
         {
---
+++ WordAnalyzer/WordSource2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace WordAnalyzer
+{
+    public class WordSource2 : IWordSource
+    {
+        private readonly IEnumerable<string> m_Text;
+
+        public WordSource2()
+        {
+            m_Text = new WordSource1().Read();
+        }
+
+        public IEnumerable<string> Read()
+        {
+            return m_Text;
+        }
+    }
+}
