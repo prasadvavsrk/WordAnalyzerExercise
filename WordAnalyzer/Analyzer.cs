4783f68f49e23014a6f792d079c3d65bf4d6125b
Geoff Bones <geoff.bones@siemens.com>
2016-03-11 13:46:22 +0000

    Add a skeleton analyzer

--- WordAnalyzer/Analyzer.cs
+++ WordAnalyzer/Analyzer.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace WordAnalyzer
 {
-    class Analyzer
+    public class Analyzer
     {
+        /// <summary>
+        /// Requests the Analyzer to analyze the input
+        /// </summary>
+        /// <param name="input"></param>
+        public Results Analyze(IEnumerable<string> input)
+        {
+            return new Results();
+        }
+
+        public class Results
+        {
+            /// <summary>
+            /// Returns the number of words in the text
+            /// </summary>
+            public int WordCount { get; set; }
+
+            /// <summary>
+            /// Returns the length of the longest word in the input
+            /// </summary>
+            public int LongestWordLength { get; set; }
+
+            /// <summary>
+            /// Returns the most commonly used words in the input. 
+            /// If two or more words share the most commonly used slot, they are all returned.
+            /// </summary>
+            public IEnumerable<string> MostCommonlyUsedWords { get; set; }
+        }
     }
 }
