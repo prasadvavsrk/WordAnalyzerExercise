a2caaeab464c50756793d4118270d7db1d1179e3
Geoff Bones <geoff.bones@siemens.com>
2016-03-11 14:23:46 +0000

    Move class to outer scope

--- WordAnalyzer/Analyzer.cs
+++ WordAnalyzer/Analyzer.cs
@@ -8,34 +8,9 @@ namespace WordAnalyzer
         /// Requests the Analyzer to analyze the input
         /// </summary>
         /// <param name="input"></param>
-        public Results Analyze(IEnumerable<string> input)
+        public AnalysisResults Analyze(IEnumerable<string> input)
         {
-            return new Results();
-        }
-
-        public class Results
-        {
-            /// <summary>
-            /// Returns the number of words in the text
-            /// </summary>
-            public int WordCount { get; set; }
-
-            /// <summary>
-            /// Returns the length of the longest word in the input
-            /// </summary>
-            public int LongestWordLength { get; set; }
-
-            /// <summary>
-            /// Returns the most commonly used words in the input. 
-            /// If two or more words share the most commonly used slot, they are all returned.
-            /// </summary>
-            public IEnumerable<string> MostCommonlyUsedWords { get; set; }
-
-            public override string ToString()
-            {
-                return string.Format("WordCount: {0}, LongestWordLength: {1}, MostCommonlyUsedWords: {2}",
-                    WordCount, LongestWordLength, string.Join(", ", MostCommonlyUsedWords ?? new string[0]));
-            }
+            return new AnalysisResults();
         }
     }
 }
